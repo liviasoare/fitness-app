73afa74ab9650d5aef0eb2974329ec05ffb75c8d
Livia Soare <liv13soare@gmail.com>
2024-08-28 16:28:03 +0300

    established models, controllers and routes

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const http = require('http');
-
-const express = require('express')
-
+// const http = require("http");
+const express = require("express");
 const app = express();
+const port = 3000;
 
-const server = http.createServer(app);
+// routes connections
+const userRoutes = require("./routes/userRoutes");
+const workoutRoutes = require("./routes/workoutRoutes");
 
-server.listen(3000)
+// *nodejs
+// const server = http.createServer(app);
+
+//* middleware to parse JSON bodies
+app.use(express.json()); 
+
+app.get("/", (req, res) => {
+  res.send("Server working!");
+});
+
+app.use('/user', userRoutes);
+app.use('/workout', workoutRoutes)
+
+// *nodejs
+// server.listen(port, port, () => {
+//     console.log('Working!!!')
+// });
+
+app.listen(port, () => {
+  console.log("Working!!!");
+});
+
+// testing
+// app.use('/static', express.static('Screenshot 2024-08-27 204000.png'))
