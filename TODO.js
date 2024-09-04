284b7874bab132fd65f71ae041cdecc4ec00f967
Livia Soare <liv13soare@gmail.com>
2024-09-04 17:12:10 +0300

    created a simple skeleton for APIs

---
+++ TODO.js
@@ -0,0 +1,23 @@
+// GET ALL WORKOUTS for a specific user by userId
+
+// GET ALL EXERCISES available
+
+// GET ALL EXERCISES by groupMuscle
+
+// GET ALL GROUPS OF MUSCLES
+
+//! DELETE workout
+
+//! DELETE exercise
+
+// ? UPDATE workout
+
+// ? UPDATE exercise
+
+//* CREATE workout
+
+//* CREATE exercise
+
+
+
+
